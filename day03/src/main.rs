b2f8fbe9046e2a899e808cdc968ed3dc49c242a5
Pablo Ramón Guevara <praguevara@gmail.com>
2023-12-04 15:51:21 +0100

    Day 03 second star

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 #[derive(Debug)]
 pub struct Board {
     rows: Vec<Vec<char>>,
@@ -16,7 +18,11 @@ impl Board {
         self.rows.get(y).and_then(|row| row.get(x))
     }
 
-    pub fn neighbours(&self, x: usize, y: usize) -> Vec<&char> {
+    pub fn valid_neighbour_indices(
+        &self,
+        x: usize,
+        y: usize,
+    ) -> impl Iterator<Item = (usize, usize)> + '_ {
         let indices = [
             (x.wrapping_sub(1), y.wrapping_sub(1)),
             (x, y.wrapping_sub(1)),
@@ -29,10 +35,15 @@ impl Board {
         ];
 
         let valid_indices = indices
-            .iter()
+            .into_iter()
             .filter(|(x, y)| *y < self.rows.len() && *x < self.rows[*y].len());
 
-        valid_indices.filter_map(|(x, y)| self.at(*x, *y)).collect()
+        valid_indices
+    }
+
+    pub fn neighbours(&self, x: usize, y: usize) -> impl Iterator<Item = &char> + '_ {
+        let valid_indices = self.valid_neighbour_indices(x, y);
+        valid_indices.filter_map(|(x, y)| self.at(x, y))
     }
 }
 
@@ -47,8 +58,7 @@ fn find_part_numbers(board: &Board) -> Vec<i32> {
                 current_number = current_number * 10 + c.to_digit(10).unwrap() as i32;
                 is_next_to_symbol |= board
                     .neighbours(x, y)
-                    .iter()
-                    .any(|&c| !c.is_ascii_digit() && *c != '.')
+                    .any(|&c| !c.is_ascii_digit() && c != '.')
             } else {
                 if is_next_to_symbol {
                     part_numbers.push(current_number);
@@ -62,10 +72,45 @@ fn find_part_numbers(board: &Board) -> Vec<i32> {
     part_numbers
 }
 
+fn find_gears(board: &Board) -> Vec<(usize, usize, i32)> {
+    let mut possible_gears: HashMap<(usize, usize), Vec<i32>> = HashMap::new(); // (x, y) -> Vec for possible gear ratio
+
+    let mut current_number = 0;
+    let mut all_neighbours: HashSet<(usize, usize)> = HashSet::new(); // Set of all neighbours of a full number
+
+    for (y, row) in board.rows.iter().enumerate() {
+        for (x, c) in row.iter().enumerate() {
+            if c.is_ascii_digit() {
+                current_number = current_number * 10 + c.to_digit(10).unwrap() as i32;
+                all_neighbours.extend(board.valid_neighbour_indices(x, y));
+            } else {
+                let gears = all_neighbours
+                    .iter()
+                    .filter(|(x, y)| *board.at(*x, *y).unwrap() == '*');
+
+                for (x, y) in gears {
+                    possible_gears
+                        .entry((*x, *y))
+                        .and_modify(|v| v.push(current_number))
+                        .or_insert_with(|| vec![current_number]);
+                }
+
+                current_number = 0;
+                all_neighbours.clear();
+            }
+        }
+    }
+
+    possible_gears
+        .into_iter()
+        .filter(|(_, v)| v.len() == 2)
+        .map(|((x, y), v)| (x, y, v[0] * v[1]))
+        .collect()
+}
+
 fn main() {
     let input = include_str!("../input.txt");
     let board = Board::new(input);
-    let numbers = find_part_numbers(&board);
-    let sum = numbers.iter().sum::<i32>();
-    println!("{}", sum);
+    let gears = find_gears(&board);
+    println!("{}", gears.iter().map(|(_, _, v)| v).sum::<i32>());
 }
