c6eae728517ad744ccc9887046a6c217148e3153
Pablo Ramón Guevara <praguevara@gmail.com>
2023-12-06 10:40:06 +0100

    Day 06 - second star

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,11 +1,11 @@
 #[derive(Debug, PartialEq, Eq, Clone)]
 struct Race {
-    time: i32,            // milliseconds
-    record_distance: i32, // millimeters
+    time: i64,            // milliseconds
+    record_distance: i64, // millimeters
 }
 
 impl Race {
-    const fn new(time: i32, record_distance: i32) -> Self {
+    const fn new(time: i64, record_distance: i64) -> Self {
         Self {
             time,
             record_distance,
@@ -13,32 +13,59 @@ impl Race {
     }
 }
 
-fn parse_input(input: &str) -> Vec<Race> {
+fn parse_input_first_part(input: &str) -> Vec<Race> {
     let mut lines = input.lines();
     let times = lines
         .next()
         .unwrap()
         .split_whitespace()
         .skip(1)
-        .map(|x| x.parse::<i32>().unwrap());
+        .map(|x| x.parse::<i64>().unwrap());
 
     let distances = lines
         .next()
         .unwrap()
         .split_whitespace()
         .skip(1)
-        .map(|x| x.parse::<i32>().unwrap());
+        .map(|x| x.parse::<i64>().unwrap());
 
     times.zip(distances).map(|(t, r)| Race::new(t, r)).collect()
 }
 
-fn compute_race_distance(race_time: i32, held_button_time: i32) -> i32 {
+fn parse_input_second_part(input: &str) -> Race {
+    let mut lines = input.lines();
+    let time = lines
+        .next()
+        .unwrap()
+        .split(':')
+        .nth(1)
+        .unwrap()
+        .split_whitespace()
+        .fold(String::new(), |a, b| format!("{}{}", a, b))
+        .parse::<i64>()
+        .unwrap();
+
+    let distance = lines
+        .next()
+        .unwrap()
+        .split(':')
+        .nth(1)
+        .unwrap()
+        .split_whitespace()
+        .fold(String::new(), |a, b| format!("{}{}", a, b))
+        .parse::<i64>()
+        .unwrap();
+
+    Race::new(time, distance)
+}
+
+fn compute_race_distance(race_time: i64, held_button_time: i64) -> i64 {
     let boat_speed = held_button_time;
     let remaining_race_time = race_time - held_button_time;
     boat_speed * remaining_race_time
 }
 
-fn ways_to_beat_record(race: &Race) -> impl Iterator<Item = i32> + '_ {
+fn ways_to_beat_record(race: &Race) -> impl Iterator<Item = i64> + '_ {
     (0..=race.time)
         .map(|held_button_time| {
             (
@@ -50,17 +77,17 @@ fn ways_to_beat_record(race: &Race) -> impl Iterator<Item = i32> + '_ {
         .map(|(held_button_time, _)| held_button_time)
 }
 
-fn total_ways_to_beat_record_product(races: impl IntoIterator<Item = Race>) -> i32 {
+fn total_ways_to_beat_record_product(races: impl IntoIterator<Item = Race>) -> i64 {
     races
         .into_iter()
-        .map(|race| ways_to_beat_record(&race).count() as i32)
+        .map(|race| ways_to_beat_record(&race).count() as i64)
         .product()
 }
 
 fn main() {
     let input = include_str!("../input.txt");
-    let races = parse_input(input);
-    println!("{}", total_ways_to_beat_record_product(races));
+    let race = parse_input_second_part(input);
+    println!("{}", ways_to_beat_record(&race).count());
 }
 
 #[cfg(test)]
@@ -72,12 +99,19 @@ mod tests {
     }
 
     #[test]
-    fn test_parse() {
+    fn test_parse_first_part() {
         let input = sample_input();
-        let races = parse_input(input);
+        let races = parse_input_first_part(input);
         assert_eq!(races[0], Race::new(7, 9));
         assert_eq!(races[1], Race::new(15, 40));
         assert_eq!(races[2], Race::new(30, 200));
+    }
+
+    #[test]
+    fn test_parse_second_part() {
+        let input = sample_input();
+        let race = parse_input_second_part(input);
+        assert_eq!(race, Race::new(71530, 940200));
     }
 
     #[test]
