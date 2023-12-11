97382c053dcf6ac4e0dc627c0a9c23e23a6de564
Pablo Ramón Guevara <praguevara@gmail.com>
2023-12-11 14:52:14 +0100

    Day 11 - second star

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,12 +1,10 @@
-use std::collections::HashSet;
+use std::collections::{BTreeSet, HashSet};
 
 type Coord = (usize, usize); // down, right
 
 #[derive(Debug)]
 struct Space {
     galaxies: HashSet<Coord>,
-    height: usize,
-    width: usize,
 }
 
 impl Space {
@@ -19,58 +17,63 @@ impl Space {
     }
 }
 
-fn parse_input(input: &str) -> Space {
-    let mut height = 0;
-    let mut width = 0;
-
+fn parse_input(input: &str, expansion_rate: usize) -> Space {
     let mut galaxies: HashSet<Coord> = HashSet::new();
     for (y, line) in input.lines().enumerate() {
         for (x, c) in line.chars().enumerate() {
             if c == '#' {
                 galaxies.insert((y, x));
             }
-            width = x + 1;
         }
-        height = y + 1;
     }
 
+    // rows
     let mut galaxies_expanded_rows = HashSet::new();
     let mut expanded_rows = 0;
-    // rows
-    for row in 0..height {
-        let row_galaxies = (0..=width)
-            .filter_map(|x| galaxies.get(&(row, x)))
-            .collect::<Vec<_>>();
-        if row_galaxies.is_empty() {
-            expanded_rows += 1;
-        } else {
-            for (y, x) in row_galaxies {
-                galaxies_expanded_rows.insert((*y + expanded_rows, *x));
-            }
+    let occuppied_rows = galaxies.iter().map(|(y, _)| *y).collect::<BTreeSet<_>>();
+
+    let row_galaxies_1 = galaxies
+        .iter()
+        .filter(|(y, _)| y == occuppied_rows.iter().next().unwrap())
+        .collect::<Vec<_>>();
+    for (y, x) in row_galaxies_1 {
+        galaxies_expanded_rows.insert((*y + expanded_rows, *x));
+    }
+
+    for (l_row, r_row) in occuppied_rows.iter().zip(occuppied_rows.iter().skip(1)) {
+        let new_rows = (expansion_rate - 1) * (r_row - l_row - 1);
+        expanded_rows += new_rows;
+        for (y, x) in galaxies.iter().filter(|(y, _)| y == r_row) {
+            galaxies_expanded_rows.insert((*y + expanded_rows, *x));
         }
     }
 
     // columns
     let mut galaxies_expanded = HashSet::new();
-    let mut expanded_columns = 0;
-    for col in 0..width + expanded_rows {
-        let column_galaxies = (0..=height + expanded_rows)
-            .filter_map(|y| galaxies_expanded_rows.get(&(y, col)))
-            .collect::<Vec<_>>();
-        if column_galaxies.is_empty() {
-            expanded_columns += 1;
-        } else {
-            for (y, x) in column_galaxies {
-                let expanded_coords = (*y, *x + expanded_columns);
-                galaxies_expanded.insert(expanded_coords);
-            }
+    let mut expanded_cols = 0;
+    let occupied_cols = galaxies_expanded_rows
+        .iter()
+        .map(|(_, x)| *x)
+        .collect::<BTreeSet<_>>();
+
+    let col_galaxies_1 = galaxies_expanded_rows
+        .iter()
+        .filter(|(_, x)| x == occupied_cols.iter().next().unwrap())
+        .collect::<Vec<_>>();
+    for (y, x) in col_galaxies_1 {
+        galaxies_expanded.insert((*y, *x + expanded_cols));
+    }
+
+    for (l_col, r_col) in occupied_cols.iter().zip(occupied_cols.iter().skip(1)) {
+        let new_cols = (expansion_rate - 1) * (r_col - l_col - 1);
+        expanded_cols += new_cols;
+        for (y, x) in galaxies_expanded_rows.iter().filter(|(_, x)| x == r_col) {
+            galaxies_expanded.insert((*y, *x + expanded_cols));
         }
     }
 
     Space {
         galaxies: galaxies_expanded,
-        height: height + expanded_rows,
-        width: width + expanded_columns,
     }
 }
 
@@ -90,7 +93,11 @@ fn compute_total_distance(space: &Space) -> usize {
 
 fn main() {
     let input = include_str!("../input.txt");
-    let space = parse_input(input);
+    let space = parse_input(input, 2);
+    let total_distance = compute_total_distance(&space);
+    println!("Total distance: {}", total_distance);
+
+    let space = parse_input(input, 1_000_000);
     let total_distance = compute_total_distance(&space);
     println!("Total distance: {}", total_distance);
 }
@@ -98,7 +105,9 @@ fn main() {
 #[test]
 fn test_distance() {
     let input = include_str!("../sample.txt");
-    let space = parse_input(input);
+    let space = parse_input(input, 2);
+
+    dbg!(&space.galaxies.iter().collect::<BTreeSet<_>>());
 
     let from_galaxy = (0, 4);
     let to_galaxy = (10, 9);
@@ -123,9 +132,25 @@ fn test_distance() {
 }
 
 #[test]
-fn test_total_distance() {
+fn test_total_distance_1() {
     let input = include_str!("../sample.txt");
-    let space = parse_input(input);
+    let space = parse_input(input, 2);
     let total_distance = compute_total_distance(&space);
     assert_eq!(total_distance, 374);
+}
+
+#[test]
+fn test_total_distance_10() {
+    let input = include_str!("../sample.txt");
+    let space = parse_input(input, 10);
+    let total_distance = compute_total_distance(&space);
+    assert_eq!(total_distance, 1030);
+}
+
+#[test]
+fn test_total_distance_100() {
+    let input = include_str!("../sample.txt");
+    let space = parse_input(input, 100);
+    let total_distance = compute_total_distance(&space);
+    assert_eq!(total_distance, 8410);
 }
