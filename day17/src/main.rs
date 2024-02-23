5124fcd1a9d49050ff510a205c42ae4b17acc85a
Pablo Ramón Guevara <praguevara@gmail.com>
2024-02-23 22:27:19 +0100

    Day 17 - first star

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,96 +1,158 @@
-use std::collections::{hash_map::Entry, HashMap, VecDeque};
+use std::{
+    cell,
+    collections::{HashMap, VecDeque},
+};
 
-use ndarray::Array2;
-
-fn parse_grid(input: &str) -> Array2<i32> {
-    let lines_vec = input
+fn parse_grid(input: &str) -> Box<[Box<[i32]>]> {
+    input
         .lines()
         .map(|l| {
             l.chars()
                 .map(|c| c.to_string().parse::<i32>().unwrap())
                 .collect::<Vec<_>>()
+                .into_boxed_slice()
         })
-        .collect::<Vec<_>>();
-
-    Array2::from_shape_vec((lines_vec.len(), lines_vec[0].len()), lines_vec.concat()).unwrap()
+        .collect::<_>()
 }
 
-fn best_path(grid: &Array2<i32>) -> (Vec<(i32, i32)>, i32) {
-    let mut visited: HashMap<(i32, i32), i32> = HashMap::new();
-    let mut queue: VecDeque<((i32, i32), Vec<(i32, i32)>, i32)> = VecDeque::new();
+#[derive(Eq, PartialEq, Hash, Clone, Copy, Debug)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
 
-    queue.push_back(((0, 0), vec![(0, 0)], grid[[0, 0]]));
-    visited.insert((0, 0), grid[[0, 0]]);
+#[derive(Eq, PartialEq, Hash, Clone, Debug)]
+struct PathInfo {
+    direction: Direction,
+    sequential: i32,
+    current_position: (i32, i32),
+}
 
-    let mut best_path = Vec::new();
-    let mut best_cost = i32::MAX;
+fn solve(grid: &[Box<[i32]>]) -> (Vec<(i32, i32)>, i32) {
+    let mut paths: HashMap<PathInfo, i32> = HashMap::new();
+    let mut best_path: Option<(PathInfo, Vec<(i32, i32)>, i32)> = None;
 
-    while let Some(((row, col), path, cost)) = queue.pop_front() {
-        if row == grid.shape()[0] as i32 - 1 && col == grid.shape()[1] as i32 - 1 {
-            if cost < best_cost {
-                best_path = path;
-                best_cost = cost;
+    let next_path_cells = |path_info: &PathInfo, cells: &[(i32, i32)]| {
+        // can't have more than 3 sequential cells
+        // can't get out of bounds
+        // can't backtrack
+
+        let (x, y) = cells.last().unwrap();
+        let mut cells = [Option::None; 4];
+
+        if !(*y == 0
+            || (path_info.sequential == 3 && path_info.direction == Direction::Up)
+            || path_info.direction == Direction::Down)
+        {
+            cells[0] = Some(((*x, y - 1), Direction::Up));
+        }
+
+        if !(*y == grid.len() as i32 - 1
+            || (path_info.sequential == 3 && path_info.direction == Direction::Down)
+            || path_info.direction == Direction::Up)
+        {
+            cells[1] = Some(((*x, y + 1), Direction::Down));
+        }
+
+        if !(*x == 0
+            || (path_info.sequential == 3 && path_info.direction == Direction::Left)
+            || path_info.direction == Direction::Right)
+        {
+            cells[2] = Some(((*x - 1, *y), Direction::Left));
+        }
+
+        if !(*x == grid[0].len() as i32 - 1
+            || (path_info.sequential == 3 && path_info.direction == Direction::Right)
+            || path_info.direction == Direction::Left)
+        {
+            cells[3] = Some(((*x + 1, *y), Direction::Right));
+        }
+
+        cells
+    };
+
+    let mut stack: Vec<(PathInfo, Vec<(i32, i32)>, i32)> = vec![(
+        PathInfo {
+            direction: Direction::Right,
+            sequential: 0,
+            current_position: (0, 0),
+        },
+        vec![(0, 0)],
+        0,
+    )];
+
+    while let Some((path_info, cells, cost)) = stack.pop() {
+        if let Some((ref _best_info, ref _best_path, best_cost)) = best_path {
+            if cost > best_cost {
+                continue;
+            }
+        }
+
+        if let Some(existing_cost) = paths.get(&path_info) {
+            if cost >= *existing_cost {
+                continue;
+            }
+        }
+
+        paths.insert(path_info.clone(), cost);
+
+        if cells.last() == Some(&(grid[0].len() as i32 - 1, grid.len() as i32 - 1)) {
+            if let Some((ref _best_info, ref _best_cells, best_cost)) = best_path {
+                if cost < best_cost {
+                    best_path = Some((path_info.clone(), cells, cost));
+                    dbg!(cost);
+                }
+            } else {
+                best_path = Some((path_info.clone(), cells, cost));
+                dbg!(cost);
             }
             continue;
         }
 
-        for (next_row, next_col) in [
-            (row + 1, col),
-            (row, col + 1),
-            (row - 1, col),
-            (row, col - 1),
-        ] {
-            if next_row >= grid.shape()[0] as i32
-                || next_col >= grid.shape()[1] as i32
-                || next_row < 0
-                || next_col < 0
-            {
-                continue;
+        for (next_cell, direction) in next_path_cells(&path_info, &cells).into_iter().flatten() {
+            let mut next_path = path_info.clone();
+            let mut next_cells = cells.clone();
+            next_cells.push(next_cell);
+            next_path.direction = direction;
+            next_path.current_position = next_cell;
+
+            if path_info.direction == direction {
+                next_path.sequential += 1;
+            } else {
+                next_path.sequential = 1;
             }
 
-            // dbg!(next_row, next_col);
-
-            let mut next_path = path.clone();
-            next_path.push((next_row, next_col));
-
-            let next_cost = cost + grid[[next_row as usize, next_col as usize]];
-            let visited_entry = visited.entry((next_row, next_col));
-            match visited_entry {
-                Entry::Occupied(mut entry) => {
-                    if next_cost < *entry.get() {
-                        entry.insert(next_cost);
-                        queue.push_back(((next_row, next_col), next_path, next_cost));
-                    }
-                }
-                Entry::Vacant(entry) => {
-                    entry.insert(next_cost);
-                    queue.push_back(((next_row, next_col), next_path, next_cost));
-                }
-            }
+            stack.push((
+                next_path,
+                next_cells,
+                cost + grid[next_cell.1 as usize][next_cell.0 as usize],
+            ));
         }
     }
 
-    (best_path, best_cost)
+    let (_, path, cost) = best_path.unwrap();
+    (path, cost)
 }
 
 fn main() {
-    let input = include_str!("../sample.txt");
+    let input = include_str!("../input.txt");
     let grid = parse_grid(input);
-    dbg!(&grid);
 
-    let (best_path, best_cost) = best_path(&grid);
+    let (cells, cost) = solve(&grid);
 
     // print best path on grid
-    for (y, row) in grid.rows().into_iter().enumerate() {
-        for (x, col) in row.into_iter().enumerate() {
-            if best_path.contains(&(y as i32, x as i32)) {
-                print!("#");
+    for (y, row) in grid.iter().enumerate() {
+        for (x, cell) in row.iter().enumerate() {
+            if cells.contains(&(x as i32, y as i32)) {
+                print!("O");
             } else {
-                print!("{}", col);
+                print!("{}", cell);
             }
         }
         println!();
     }
 
-    dbg!(&best_cost);
+    dbg!(&cost);
 }
